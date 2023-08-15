886a38a7a13116edd114bfc12573ce0ca8878e2f
lalo532 <molusco636@gmail.com>
2023-08-15 16:30:50 -0600

    [ADD]componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Home', component:HomeComponent},
+  {path:'Projects',component:ProjectsComponent},
+  {path:'About-us',component:AboutUsComponent},
+  {path:'Services',component:ServicesComponent},
+  {path:'Contact',component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
