6ec0c7b370b782f2d73ed470bf18d71664cf6778
lalo532 <molusco636@gmail.com>
2023-08-15 16:49:12 -0600

    [changes]Routes
    Cambios en rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,20 @@ import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ServicesComponent } from './services/services.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 const routes: Routes = [
-  { path: 'Home', component:HomeComponent},
+  { path:'Home', component:HomeComponent},
   {path:'Projects',component:ProjectsComponent},
   {path:'About-us',component:AboutUsComponent},
   {path:'Services',component:ServicesComponent},
-  {path:'Contact',component:ContactComponent}
+  {path:'Contact',component:ContactComponent},
+  {path:'**',redirectTo: "home"}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
