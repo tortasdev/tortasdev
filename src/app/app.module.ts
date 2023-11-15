6af6e3a4a439d52c2da7fb98aebce81421ed5c57
carlostisca <carlose.gonlez17z@gmail.com>
2023-11-14 22:43:49 -0600

    ADDED: BUTTONS LANG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import 'boxicons';
-
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+// import ngx-translate and the http loader
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './footer/footer.component';
-// import ngx-translate and the http loader
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -29,7 +28,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
     ProjectsComponent,
     ServicesComponent,
     ContactComponent,
-    FooterComponent    
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import 'boxicons';
+
+import { constants } from 'src/common/constants';
+
+const { LANGUAGE_DEFAULT } = constants;
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(LANGUAGE_DEFAULT);
+    translate.use(LANGUAGE_DEFAULT);
+  }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+}
